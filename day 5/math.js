4122c8298cff393b7692f1164ab97fdad40b4373
talha-bhutta-invozone <talha.bhutta@invozone.com>
2022-04-29 21:49:52 +0500

    day 5 math properties and methods

---
+++ day 5/math.js
@@ -0,0 +1,71 @@
+
+//Math Properties
+
+//eulers constant
+var property_value = Math.E
+document.write("Property Value is :" + property_value + "<br/>");
+
+//logarithm of 2
+var property_value = Math.LN2
+document.write("Property Value is :" + property_value + "<br/>");
+
+//logarithm of 10
+var property_value = Math.LN10
+document.write("Property Value is :" + property_value + "<br/>");
+
+//logarithm of 2 base
+var property_value = Math.LOG2E
+document.write("Property Value is :" + property_value + "<br/>");
+
+//value of PI
+var property_value = Math.PI
+document.write("Property Value is :" + property_value + "<br/>");
+
+//square root of 1/2
+var property_value = Math.SQRT1_2
+document.write("Property Value is :" + property_value + "<br/>");
+
+//square root of 1/2
+var property_value = Math.SQRT4
+document.write("Property Value is :" + property_value + "<br/>");
+var value = Math.abs(-1);
+document.write("First Test Value : " + value);
+
+var value = Math.abs(null);
+document.write("<br />Second Test Value : " + value);
+
+var value = Math.abs(20);
+document.write("<br />Third Test Value : " + value);
+
+var value = Math.abs("string");
+document.write("<br />Fourth Test Value : " + value);
+
+var value = Math.acos(null);
+document.write("<br />Second Test Value : " + value);
+
+var value = Math.acos(30);
+document.write("<br />Third Test Value : " + value);
+
+var value = Math.acos("string");
+document.write("<br />Fourth Test Value : " + value);
+document.write("First Test Value : " + value);
+
+var value = Math.random();
+document.write("<br />Second Test Value : " + value);
+
+var value = Math.random();
+document.write("<br />Third Test Value : " + value);
+
+var value = Math.random();
+document.write("<br />Fourth Test Value : " + value);
+var value = Math.round(0.5);
+document.write("First Test Value : " + value);
+
+var value = Math.round(20.7);
+document.write("<br />Second Test Value : " + value);
+
+var value = Math.round(20.3);
+document.write("<br />Third Test Value : " + value);
+
+var value = Math.round(-20.3);
+document.write("<br />Fourth Test Value : " + value); 
